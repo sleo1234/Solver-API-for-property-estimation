e876c09d915d0b5c75f85153b7ddd9e1f4887b1b
sleo1234 <safta.leonard@yahoo.com>
2023-08-23 22:34:24 +0300

    styling table for components

--- src/main/resources/static/js/flash.js
+++ src/main/resources/static/js/flash.js
@@ -44,13 +44,23 @@ function getComponents() {
        function selectComponents(){
 
        var selected=[];
-
+       var counter =0;
        $("#addButton").on("click", function(){
-
+       table = $("#myTable");
         var name = $("input[name='Search component']").val();
         selected.push(name);
+        counter++;
+        table.append('<tr id="row"' +string.valueOf(counter)+'><td>'+name+'</td ><td contenteditable="true"></td><td><span class="table-remove"><button type="button" id ="removeButton" class="btn btn-danger btn-rounded btn-sm my-0"> Remove </button></span></td></tr>');
+
+
+      $("#removeButton").on("click", function(){
+
+                          document.getElementById("row"+string.valueOf(counter)).remove();
+                              console.log(document.getElementById("myTable"));
+                 })
 
        });
        selectedComponents = [...selected];
-        alert(selectedComponents);
+        //alert(selectedComponents);
         }
+
