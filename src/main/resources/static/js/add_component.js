808a0eead8a9e1406273d2695cfb6f17991900e1
sleo1234 <safta.leonard@yahoo.com>
2024-08-07 23:03:25 +0300

    adding jquary function to populate body for POST component API

--- src/main/resources/static/js/add_component.js
+++ src/main/resources/static/js/add_component.js
@@ -8,13 +8,26 @@ $(document).ready(function() {
   var body={}
 
 
-  $("#form-component").find("input[type='text']").each(function(){
-      console.log("****** "+$(this).val()+" "+$(this).attr('id'));
-  })
+      $("#form-component").find("input[type='text']").each(function(){
+      reqId = $(this).attr('id')
+      reqVal = $(this).val()
+
+      if (isNaN($(this).val())) {
+         body[$(this).attr('id')] = $(this).val()
+        }
+        else{
+        if ($(this).val() == ""){
+           body[$(this).attr('id')] = 0.0
+        }
+        body[$(this).attr('id')] = parseFloat($(this).val())
+        }
+
+      })
 
 
-
-
+       for (c in body){
+          console.log(c+" -> "+body[c])
+       }
 
 
     });
